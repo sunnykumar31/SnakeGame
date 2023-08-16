abf3dad39e29b089babd5151867fdb94c642f4d2
sunny kumar <jaissunny158@gmail.com>
2023-08-16 17:39:22 +0530

    1stCommit

---
+++ script.js
@@ -0,0 +1,141 @@
+const canvas=document.getElementById('canvas');
+// const pen=canvas.getContext('2d');
+const pen = canvas.getContext('2d');
+pen.fillStyle='yellow';
+
+const cs=25;
+const H=650;
+const W=1200;
+let food=null;
+let score=0;
+
+const Snake={
+    inlen:3,
+    cells:[],
+    direction:'right',
+    CreateSnake:function(){
+        for(let i=0;i<this.inlen;i++){
+            this.cells.push({
+                x:i,
+                y:0
+            });
+        }
+    },
+    DrawSanke:function(){
+        for(let cell of this.cells){
+            pen.fillRect(cell.x*cs,cell.y*cs,cs-1,cs-1);
+        }
+        
+    },
+    UpdateSnake:function(){
+        let headX=this.cells[this.cells.length-1].x;
+        let headY=this.cells[this.cells.length-1].y;
+        if(headX===food.x && headY===food.y){
+            score++;
+            food=getRandomfood();
+        }
+        else{
+            this.cells.shift();
+        }
+        let nextX;
+        let nextY;
+        if(this.direction==='up'){
+            nextX=headX;
+            nextY=headY-1;
+            if(nextY*cs<0){
+                pen.fillStyle='red';
+                pen.fillText('Game Over',500,350);
+                clearInterval(id);
+            }
+        }
+        else if(this.direction==='down'){
+            nextX=headX;
+            nextY=headY+1;
+            if(nextY*cs>=H){
+                pen.fillStyle='red';
+                pen.fillText('Game Over',500,350);
+                clearInterval(id);
+            }
+        }
+        else if(this.direction==='right'){
+            nextX=headX+1;
+            nextY=headY;
+            if(nextX*cs>=W){
+                pen.fillStyle='red';
+                pen.fillText('Game Over',500,350);
+                clearInterval(id);
+            }
+        }
+        else{
+            nextX=headX-1;
+            nextY=headY;
+            if(nextX*cs<0){
+                pen.fillStyle='red';
+                pen.fillText('Game Over',500,350);
+                clearInterval(id);
+            }
+        }
+        
+        this.cells.push({
+            x:nextX,
+            y:nextY
+        }); 
+    }
+
+
+
+}
+
+function init(){
+    Snake.CreateSnake();
+    food=getRandomfood();
+    document.addEventListener('keydown',(e)=>{
+        if(e.key==='ArrowDown'){
+            Snake.direction='down';
+        }
+        else if(e.key==='ArrowUp'){
+            Snake.direction='up';
+        }
+        else if(e.key==='ArrowRight'){
+            Snake.direction='right';
+        }
+        else if(e.key==='ArrowLeft'){
+            Snake.direction='left';
+        }
+        console.log(Snake.direction);
+    });
+}
+
+function Draw(){
+    pen.clearRect(0, 0, W, H);
+    pen.font = '40px sans-serif';
+    // pen.fillStyle='blue';
+    // pen.fillText(`score ${score}`,30,30);
+    pen.fillText(`Score ${score}`, 50, 30);
+    pen.fillStyle = 'blue';
+    pen.fillRect(food.x * cs, food.y * cs, cs, cs);
+    // pen.clearRect(0,0,W,H);
+    pen.fillStyle='yellow';
+    Snake.DrawSanke();
+}
+
+function Update(){
+    Snake.UpdateSnake();
+}
+
+function gameLoop(){
+    Draw();
+    Update();
+    
+}
+function getRandomfood(){
+    const foodX=Math.round(Math.random()*(W-cs)/cs);
+    const foodY=Math.round(Math.random()*(H-cs)/cs);
+    food={
+        x:foodX,
+        y:foodY
+    }
+    return food;
+}
+init();
+const id=setInterval(gameLoop,100);
